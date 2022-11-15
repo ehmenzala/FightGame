c19d86081ca78eab285fdfd3cf90e8f9858079e3
Esteban Huaman <ehmenzala@gmail.com>
2022-11-14 19:55:58 -0500

    Modified Dice enum

--- src/main/java/com/ehmenzala/enums/Dice.java
+++ src/main/java/com/ehmenzala/enums/Dice.java
@@ -3,22 +3,21 @@ package com.ehmenzala.enums;
 import java.io.File;
 
 public enum Dice {
+    ONE     ("./images/dice-1.png"),
+    TWO     ("./images/dice-2.png"),
+    THREE   ("./images/dice-3.png"),
+    FOUR    ("./images/dice-4.png"),
+    FIVE    ("./images/dice-5.png"),
+    SIX     ("./images/dice-6.png");
     
-    ONE     (new File("./images/dice-1.png")),
-    TWO     (new File("./images/dice-2.png")),
-    THREE   (new File("./images/dice-3.png")),
-    FOUR    (new File("./images/dice-4.png")),
-    FIVE    (new File("./images/dice-5.png")),
-    SIX     (new File("./images/dice-6.png"));
-    
-    private File imageFile;
+    private String imagePath;
 
-    private Dice(File imageFile) {
-        this.imageFile = imageFile;
+    private Dice(String imagePath) {
+        this.imagePath = imagePath;
     }
 
-    public File getFaceImage() {
-        return imageFile;
+    public String getImagePath() {
+        return imagePath;
     }
     
     // Get random face
