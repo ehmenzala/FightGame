182cc8a057bcc55dba9c55cc9f108ba862b8bb7f
Esteban Huaman <ehmenzala@gmail.com>
2022-12-17 20:55:07 -0500

    Modify Main class

--- src/main/java/com/ehmenzala/app/Main.java
+++ src/main/java/com/ehmenzala/app/Main.java
@@ -3,21 +3,10 @@ package com.ehmenzala.app;
 public class Main {
 
     public static void main(String[] args) {
-        initQuestionPool();
-        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                //new FightGame.MainMenu().setVisible(true);
-                //new FightGame.TriviaGame().setVisible(true);
-                //new FightGame.FighterData().setVisible(true);
-                new FightGame.DeathMatch().setVisible(true);
+                 new FightGame.MainMenu().setVisible(true);
             }
         });
-    }
-
-    public static void initQuestionPool() {
-
-        // Options
-        
     }
 }
